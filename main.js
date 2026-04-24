5699d6aaf5980a115b551919c91c249fa83c18ed
hanjoojn <hanjoojn@gmail.com>
2026-04-24 14:27:41 -0700

    Update main.js

--- main.js
+++ main.js
@@ -9,9 +9,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function navToggle() {
   var x = document.getElementById("mobileHeader");
+  var y = document.getElementById("mobileHeaderBtn");
   if (x.style.display === "grid") {
     x.style.display = "none";
   } else {
     x.style.display = "grid";
+  }
+  if (y.style.position === "sticky") {
+    y.style.display = "fixed";
+  } else {
+    y.style.display = "sticky";
   }
 }
